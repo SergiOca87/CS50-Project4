b3c7791958fffce2826067b0edb8909caad445fa
Sergi <ocasergi@gmail.com>
2020-09-05 02:57:56 +0200

    first commit

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+// API Calls - GET
+// /emails/<mailbox> where mailbox is either inbox, sent or archive
+// GET /emails/<int:email_id> for a single email
+
+fetch('/emails/inbox')
+.then(response => response.json())
+.then(emails => {
+    // Print emails
+    console.log(emails);
+
+    // ... do something else with emails ...
+});
+
+// API Calls - POST
+// This needs the recipient, subject and body
+fetch('/emails', {
+  method: 'POST',
+  body: JSON.stringify({
+      recipients: 'baz@example.com',
+      subject: 'Meeting time',
+      body: 'How about we meet tomorrow at 3pm?'
+  })
+})
+.then(response => response.json())
+.then(result => {
+    // Print result
+    console.log(result);
+});
+
+// API Calls - PUT to mark emails as read/unread archived/unarchived
+fetch('/emails/100', {
+  method: 'PUT',
+  body: JSON.stringify({
+      archived: true
+  })
+})
