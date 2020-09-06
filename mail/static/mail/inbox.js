bacd3f527a95f4b1c94d0b353ea642a1639d1699
Sergi <ocasergi@gmail.com>
2020-09-06 22:24:42 +0200

    all specifications

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -15,12 +15,82 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#single-email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+}
+
+function renderEmail(id, mailbox) {
+  document.querySelector('#single-email-view').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
+  const singleEmailView = document.querySelector('#single-email-view .mail-container');
+  const archive = `
+    <button class="btn archive">Archive</button>
+  `;
+  const unArchive = `
+    <button class="btn unarchive">Unarchive</button>
+  `;
+
+  // Fetch the specific e-mail using the id passed in as a parameter
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(result => {
+    // Object destructuring assingment
+    const {subject, sender, recipients, body, read} = result;
+
+    singleEmailView.innerHTML = `
+      <div class="single-email">  
+        <h3>${subject}</h3>
+        <p><strong>Sender</strong>: ${sender}</p>
+        <p><strong>Recipient/s</strong>: ${recipients.join(',')}</p>
+        <p><strong>Message</strong>: ${body}</p>
+      </div>
+      `;
+  })
+
+  // Dry those up!
+  .then(() => {
+    if( mailbox === 'inbox' ) {
+      document.querySelectorAll('.single-email').forEach((email) => {
+        email.insertAdjacentHTML('beforeend', archive)
+        email.querySelector('.btn').addEventListener('click', function() {
+          fetch(`/emails/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+              archived: true
+            })
+          })
+          load_mailbox('inbox')
+        })
+      })
+    } else if( mailbox === 'archive' ) {
+      console.log('archive', mailbox)
+      document.querySelectorAll('.single-email').forEach((email) => {
+        email.insertAdjacentHTML('beforeend', unArchive)
+        email.querySelector('.btn').addEventListener('click', function() {
+          fetch(`/emails/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+              archived: false
+            })
+          })
+          load_mailbox('inbox')
+        })
+      })
+    }
+  })
+
+  // If an email is clicked, also mark it as read
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })
 }
 
 function load_mailbox(mailbox) {
@@ -30,6 +100,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   emailsView.style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#single-email-view').style.display = 'none';
 
   // Show the mailbox name
   emailsView.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -39,51 +110,39 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(result => {
-      // Print emails
-      console.log(result);
-      result.forEach((mail) => {
-        
-        const mailTemplate =`
-          <a href="/emails/${mail.id}">
-            <div class="card">
-              <p><strong>Sender</strong>: ${mail.sender}</p>
-              <p><strong>Recipients</strong>: ${mail.recipients.join(',')}</p>
-            </div> 
-          </a>
-        `
-
-        emailsView.insertAdjacentHTML('beforeend', mailTemplate)
-      })
-
+    // Print emails
+    console.log(result);
+    result.forEach((mail) => {
+      // Read or Unread instead of true or false for legibility, as it used as a class for styling purposes
+      const read = mail.read === true ? 'read' : 'unread' 
       
+      const mailTemplate =`
+        <div class="card email ${read}" data-id=${mail.id}>
+          <p><strong>Sender</strong>: ${mail.sender}</p>
+          <p><strong>Recipients</strong>: ${mail.recipients.join(',')}</p>
+        </div> 
+      `
+      emailsView.insertAdjacentHTML('beforeend', mailTemplate)
+    })
+
+    // Add event listeners to each email to load the single email view
+    document.querySelectorAll('.email').forEach((email) => {
+      email.addEventListener('click', function(){
+        return renderEmail( this.dataset.id, mailbox );
+      })
+    })
   });
 }
-
-      // foreach e-mail create an Element,
-
-      // If the email is unread, it should appear with a white background.
-
-      // If the email has been read, it should appear with a gray background.
-
-      // Each element should be clickable, surround by <a>
-      // The URL should be for the single view (Djando, int?)
-
-      // append that element
-      
 
 function submit_form(e) {
   console.log(e)
   e.preventDefault();
 
   // Capture values
-  // recipients, subject, and body
   const recipients = document.querySelector('#compose-recipients').value;
   const subject = document.querySelector('#compose-subject').value;
   const body = document.querySelector('#compose-body').value;
 
-  // Send the values via Fetch to the API
-
-  // All recipients must also be valid users who have registered on this particular web application
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
@@ -94,49 +153,7 @@ function submit_form(e) {
   })
   .then(response => response.json())
   .then(result => {
-      // Print result
       console.log(result);
       load_mailbox('sent')
   });
-  
-
-  // load the user’s sent mailbox.
-
 }
-
-// API Calls - GET
-// /emails/<mailbox> where mailbox is either inbox, sent or archive
-// GET /emails/<int:email_id> for a single email
-
-// fetch('/emails/inbox')
-// .then(response => response.json())
-// .then(emails => {
-//     // Print emails
-//     console.log(emails);
-
-//     // ... do something else with emails ...
-// });
-
-// API Calls - POST
-// This needs the recipient, subject and body
-// fetch('/emails', {
-//   method: 'POST',
-//   body: JSON.stringify({
-//       recipients: 'baz@example.com',
-//       subject: 'Meeting time',
-//       body: 'How about we meet tomorrow at 3pm?'
-//   })
-// })
-// .then(response => response.json())
-// .then(result => {
-//     // Print result
-//     console.log(result);
-// });
-
-// API Calls - PUT to mark emails as read/unread archived/unarchived
-// fetch('/emails/100', {
-//   method: 'PUT',
-//   body: JSON.stringify({
-//       archived: true
-//   })
-// })
